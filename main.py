6fa00f4c15bfdeca19566ed46dfc8b7208523e17
gabrielbaute <gabrielbaute@gmail.com>
2026-02-20 13:49:48 -0400

    Entrypoint para ejecutar la api, tomamos el mismo que nos había generado uv al crear el proyecto... a ver qué tal...

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from octopus-photos!")
+""" 
+Entrypoint de la API
+"""
+import uvicorn
 
+from app.settings import settings, OctopusLogger
+from app.api.app_factory import create_app
+from app.database.db_config import init_db
+
+# Nos aseguramos que los directorios se crean
+settings.ensure_dirs()
+
+# Inicializamos el logger
+OctopusLogger.setup_logging(level="INFO")
+
+# Creamos la base de datos
+init_db(settings=settings)
+
+# Creamos la app de la API
+app = create_app(settings=settings)
+
+def run_server():
+    """
+    Run the FastAPI server.
+    """
+    uvicorn.run(
+        "main:app",
+        host=settings.API_HOST,
+        port=settings.API_PORT,
+        log_level="info",
+        reload=False,
+    )
 
 if __name__ == "__main__":
-    main()
+    run_server()
