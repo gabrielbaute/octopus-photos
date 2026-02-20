b37237a38b8630e84a3252341ba89ea176afc42d
gabrielbaute <gabrielbaute@gmail.com>
2026-02-20 13:49:04 -0400

    App Factory, aquí ensamblamos toda la API, tenemos algunas dependencias para carga de archivos estáticos que luego usaremos para un minifront, pero de momento aun no decido qué voy a hacer con eso

---
+++ app/api/app_factory.py
@@ -0,0 +1,40 @@
+"""
+FastAPI Application Factory module
+"""
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles #Para archivos estáticos en un futuro, aunque el frontend irá en flutter
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.settings import Settings
+from app.api.include_routes import include_routes
+
+def create_app(settings: Settings) -> FastAPI:
+    """
+    Crea y configura la aplicación FastAPI.
+    
+    Args:
+        settings (Settings): Las configuraciones de la aplicación.
+    
+    Returns:
+        FastAPI: Instancia de la aplicación FastAPI configurada.
+    """
+    app = FastAPI(
+        title=settings.APP_NAME,
+        description=f"{settings.APP_NAME} API",
+        version=settings.APP_VERSION,
+        docs_url="/docs",
+        redoc_url="/redoc",
+        openapi_url="/openapi.json",
+    )
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    include_routes(app, prefix="/api/v1")
+
+    return app
