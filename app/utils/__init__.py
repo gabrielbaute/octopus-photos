38fc34d5542811aee0fbae2a11c9bb4eb937d500
gabrielbaute <gabrielbaute@gmail.com>
2026-02-26 09:59:52 -0400

    Creamos una utilidad que busca las variables de entorno en diversos lugares y que toma en cuenta si el script principal se ejecuta en frozen, desde pyinstaller, o desplegado como un archivo python, como podria ser en docker o en desarrollo

---
+++ app/utils/__init__.py
@@ -0,0 +1,2 @@
+from app.utils.dates import get_now
+from app.utils.get_environment_path import get_env_paths
---
+++ app/utils/get_environment_path.py
@@ -0,0 +1,15 @@
+import sys
+from typing import Tuple
+from pathlib import Path
+
+def get_env_paths() -> Tuple[Path, ...]:
+    """
+    Retorna una jerarquía de rutas donde buscar el archivo .env.
+    """        
+    user_path = Path.home() / ".OctopusPhotos" / "config" / ".env"
+    local_path = Path(__file__).resolve().parent.parent.parent / ".env"
+    
+    if getattr(sys, 'frozen', False):
+        return (user_path,)
+    
+    return (user_path, local_path)
